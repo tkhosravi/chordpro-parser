f69277c7e6118a3b54239cf7203c22cc040c7d33
Tony Khosravi <tony.khosravid@gmail.com>
2019-05-06 11:13:22 +0200

    Init node files

---
+++ index.js
@@ -0,0 +1,26 @@
+const ChordSheetJS = require('chordsheetjs').default;
+
+const format = {
+  HTML: "html",
+  HTML_DIV: "htmldiv",
+  HTML_TABLE: "htmltable",
+  TEXT: "text"  
+};
+
+module.exports.parse(chordproSheet, returnFormat) {
+  const parser = new ChordSheetJS.ChordProParser().default;
+  const song = parser.parse(chordSheet);
+  returnFormat = (typeof returnFormat === 'undefined' ) ? format.TEXT : returnFormat.toLowerCase();
+  var formatter;
+  switch (returnFormat) {
+	case "html","htmldiv":
+	  formatter = new ChordSheetJS.HtmlDivFormatter();
+	  break;
+	case "htmltable":
+	  formatter = new ChordSheetJS.HtmlTableFormatter();
+	  break;
+	default:
+	  formatter = new ChordSheetJS.TextFormatter();
+  }
+  return formatter.format(song);
+}
